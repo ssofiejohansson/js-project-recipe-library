6844c9fe4136dd18137b2db160a127518d201433
Sofie <sofieannamatilda@hotmail.se>
2025-02-27 20:53:16 +0100

    changed input types and linked js code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # js-project-recipe-library
+
+Link to netlify: https://stirring-yeot-236514.netlify.app/
--- js/script.js
+++ js/script.js
@@ -1,39 +1,78 @@
-const pressButton = () => {
-  const veganBtn = document.getElementById("veganBtn");
-  const vegetarianBtn = document.getElementById("vegetarianBtn");
-  const glutenFreeBtn = document.getElementById("glutenFreeBtn");
-  const dairyFreeBtn = document.getElementById("dairyFreeBtn");
-  const allDietsBtn = document.getElementById("allDietsBtn");
-  const addDietType = document.getElementById("dietType");
+// defining my variables
 
-  const updateDietType = (diet) => {
-    addDietType.innerHTML = `<p>${diet}</p>`;
-  };
+const veganBtn = document.querySelector('#veganBtn input')
+const vegetarianBtn = document.querySelector('#vegetarianBtn input')
+const glutenFreeBtn = document.querySelector('#glutenFreeBtn input')
+const dairyFreeBtn = document.querySelector('#dairyFreeBtn input')
+const dietChoice = document.querySelector('#dietChoice')
 
-  allDietsBtn.addEventListener("click", () => {
-    console.log("All button is pressed");
-    updateDietType("All diets");
-  });
+const under15min = document.querySelector('#under15minBtn input')
+const between15and30min = document.querySelector('#between15and30minBtn input')
+const between30and60min = document.querySelector('#between30and60minBtn input')
+const over60min = document.querySelector('#over60minBtn input')
+const cookingTime = document.querySelector('#timeChoice')
 
-  veganBtn.addEventListener("click", () => {
-    console.log("Vegan button is pressed");
-    updateDietType("Vegan");
-  });
+const mostPopular = document.querySelector('#mostPopular input')
+const leastPopular = document.querySelector('#leastPopular input')
+const sort = document.querySelector('#sort')
 
-  vegetarianBtn.addEventListener("click", () => {
-    console.log("Vegetarian button is clicked");
-    updateDietType("Vegetarian");
-  });
+//naming functions
 
-  dairyFreeBtn.addEventListener("click", () => {
-    console.log("dairyfree button is clicked");
-    updateDietType("Dairy-free");
-  });
+const updateDietChoice = () => {
+  dietChoice.innerHTML = "" // This is to clear the div from the start
 
-  glutenFreeBtn.addEventListener("click", () => {
-    console.log("glutenfree button is clicked");
-    updateDietType("Gluten-free");
-  });
-};
+  if (veganBtn.checked) {
+    dietChoice.innerHTML += `<p>🥑 Vegan vibes only! Taste the goodness of plant-based meals.</p>`
+  }
+  if (vegetarianBtn.checked) {
+    dietChoice.innerHTML += `<p>🥦 Go green! Discover vibrant and delicious vegetarian recipes for a plant-powered day.</p>`
+  }
+  if (glutenFreeBtn.checked) {
+    dietChoice.innerHTML += `<p>❌🍞 No gluten, no problem! Delicious gluten-free recipes coming your way.</p>`
+  }
+  if (dairyFreeBtn.checked) {
+    dietChoice.innerHTML += `<p>🚫🥛 Explore creamy, dreamy dairy-free recipes.</p>`
+  }
+}
 
-pressButton();
+const updateCookingTime = () => {
+  cookingTime.innerHTML = ""
+
+  if (under15min.checked) {
+    cookingTime.innerHTML += `<p>⏱️ Quick and tasty! Under 15 minutes of cooking, because time is precious.</p>`
+  }
+  if (between15and30min.checked) {
+    cookingTime.innerHTML += `<p>⏲️ Just the right amount of time! Perfect for a maximum 30 minute cooking adventure.</p>`
+  }
+  if (between30and60min.checked) {
+    cookingTime.innerHTML += `<p>🕒 Got a bit more time? Whip up something delicious in less than an hour.</p>`
+  }
+  if (over60min.checked) {
+    cookingTime.innerHTML += `<p>⏳ Slow-cooked perfection! Over 60 minutes for those who love to take their time.</p>`
+  }
+}
+
+const sortReceipes = () => {
+  sort.innerHTML = ""
+
+  if (mostPopular.checked) {
+    sort.innerHTML += `<p>🔥 Hot right now! Check out the most popular recipes everyone’s raving about:</p>`
+  }
+  if (leastPopular.checked) {
+    sort.innerHTML += `<p>👀 The underdogs! These recipes might be hidden gems:</p>`
+  }
+}
+
+// Eventlisteners always at the end
+veganBtn.addEventListener("change", updateDietChoice)
+vegetarianBtn.addEventListener("change", updateDietChoice)
+glutenFreeBtn.addEventListener("change", updateDietChoice)
+dairyFreeBtn.addEventListener("change", updateDietChoice)
+
+under15min.addEventListener("change", updateCookingTime)
+between15and30min.addEventListener("change", updateCookingTime)
+between30and60min.addEventListener("change", updateCookingTime)
+over60min.addEventListener("change", updateCookingTime)
+
+mostPopular.addEventListener("change", sortReceipes)
+leastPopular.addEventListener("change", sortReceipes)
